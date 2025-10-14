773f65129a18e9da1d879947e282072e0ddf9c96
abhi-rzoro <abhayparasharr@gmail.com>
2025-10-14 10:32:00 -0700

    Convert the initial version of the game into Godot

--- game/scenes/main_menu.gd
+++ game/scenes/main_menu.gd
@@ -1,12 +1,11 @@
 extends Control
 
 
-func _on_player_two_button_pressed():
-	GameManager.playerNumber = 1
-	SceneManager.change_scene("res://scenes/main_game.tscn")
-
-
 func _on_player_one_button_pressed():
+	GameManager.playerNumber = 1
+	SceneManager.change_scene("res://scenes/game.tscn")
+
+
+func _on_player_two_button_pressed():
 	GameManager.playerNumber = 2
-	SceneManager.change_scene("res://scenes/main_game.tscn")
-	pass # Replace with function body.
+	SceneManager.change_scene("res://scenes/game.tscn")
