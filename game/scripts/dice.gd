d1a1b5cce0c049420056dfb6fb54e64f27c8f8e4
Joseph-Jupyter <josephjeemail@gmail.com>
2025-11-03 13:10:16 -0800

    added effect for dice 1

--- game/scripts/dice.gd
+++ game/scripts/dice.gd
@@ -1,6 +1,9 @@
 class_name Dice
 extends Node2D
 
+
+signal on_dice_1_destroyed
+
 @onready var sprite = $AnimatedSprite2D as AnimatedSprite2D
 
 var score_value: int = 0:
@@ -23,4 +26,10 @@ func get_score_value() -> int:
 
 
 func destroy() -> void:
+	destroyed_effect()
 	queue_free()
+
+
+func destroyed_effect() -> void:
+	if score_value == 1:
+		on_dice_1_destroyed.emit()
