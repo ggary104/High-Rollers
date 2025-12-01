55b3966335313cb0e48f8756286782157ece689e
Joseph-Jupyter <josephjeemail@gmail.com>
2025-12-01 13:03:25 -0800

    Worked on player ui

--- game/scripts/dice_tile.gd
+++ game/scripts/dice_tile.gd
@@ -11,6 +11,7 @@ var dice: Dice = null:
 		dice = value
 		if not is_instance_valid(dice):
 			return
+		
 		dice.on_dice_destroyed.connect(remove_dice)
 		dice.is_placed = true
 
@@ -27,14 +28,9 @@ func _on_area_2d_mouse_exited() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("click") and is_touching_mouse:
-		print("Dice:")
-		print(dice)
-		print("Enabled:")
-		print(enabled)
 	if event.is_action_pressed("click") and is_touching_mouse and enabled:
 		select()
-		print("select")
+		print(size)
 
 
 func select() -> void:
